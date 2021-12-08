e2f5f5f19b678945b5ea224cef04d9d57d32eef9
mrs1669 <mrs1669.develop@gmail.com>
2021-12-08 11:16:10 +0900

    setup table viewe

--- numer0n-analysis/numer0n-analysis/ViewController.swift
+++ numer0n-analysis/numer0n-analysis/ViewController.swift
@@ -14,17 +14,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var historyTableView: UITableView = UITableView()
     
+    var historyArray: [Int] = [0123, 4567, 8901]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+        historyTableView.frame = CGRect(x:screenWidth * 0/100, y:screenHeight * 10/100, width:screenWidth * 100/100, height:screenHeight * 80/100)
         historyTableView.register(UITableViewCell.self, forCellReuseIdentifier: "historyCell")
+        self.view.addSubview(historyTableView)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return historyArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "historyCell")
+        cell.textLabel?.text = String(historyArray[indexPath.row])
         return cell
     }
 }
